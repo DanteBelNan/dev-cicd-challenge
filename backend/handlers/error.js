5bbf12e5b47c078507ec0d2a84a55a4c9af54eb1
DanteBelNan <dantebelnan@gmail.com>
2025-10-16 18:16:49 -0300

    temp error removed, making fail in E2E testing so rollback goes there!

--- backend/handlers/error.js
+++
@@ -1,3 +0,0 @@
-export const createError = (_req, res) => {
-  res.status(500).json({ mustRollback: true, message: 'This is a simulated production error.' });
-};
--- backend/handlers/health.js
+++ backend/handlers/health.js
@@ -1,3 +1,3 @@
 export const getHealth = (_req, res) => {
-  res.status(200).json({ status: 'ok. This content is not gonna be available since an automatic rollback will go to the previous version' });
+  res.status(200).json({ status: 'ok' });
 };
